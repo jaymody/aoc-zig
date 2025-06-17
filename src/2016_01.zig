02faf55d09313a2e20c4f4cd26701faee5219f1f
Jay Mody <jaykmody@gmail.com>
2025-06-16 21:15:28 -0400

    Refactor.

--- src/2016_01.zig
+++ src/2016_01.zig
@@ -21,23 +21,28 @@ fn rotate_right(direction: Direction) Direction {
     };
 }
 
+fn parseInstruction(instruction: []const u8, direction: Direction) !struct { Direction, i64 } {
+    const new_direction = switch (instruction[0]) {
+        'R' => rotate_right(direction),
+        'L' => rotate_left(direction),
+        else => std.debug.panic("failed to parse instruction: {s}\n", .{instruction}),
+    };
+
+    const distance = try std.fmt.parseInt(i64, instruction[1..], 10);
+
+    return .{ new_direction, distance };
+}
+
 fn part1(buffer: []const u8, _: std.mem.Allocator) !u64 {
     var instructions = std.mem.splitSequence(u8, buffer, ", ");
 
     var direction = Direction.up;
+    var distance: i64 = undefined;
     var dx: i64 = 0;
     var dy: i64 = 0;
 
     while (instructions.next()) |instruction| {
-        if (instruction[0] == 'R') {
-            direction = rotate_right(direction);
-        } else if (instruction[0] == 'L') {
-            direction = rotate_left(direction);
-        } else {
-            std.debug.panic("failed to parse instruction: {s}\n", .{instruction});
-        }
-
-        const distance = try std.fmt.parseInt(i64, instruction[1..], 10);
+        direction, distance = try parseInstruction(instruction, direction);
         switch (direction) {
             Direction.up => dy = dy + distance,
             Direction.down => dy = dy - distance,
@@ -53,23 +58,16 @@ fn part2(buffer: []const u8, allocator: std.mem.Allocator) !u64 {
     var instructions = std.mem.splitSequence(u8, buffer, ", ");
 
     var direction = Direction.up;
+    var distance: i64 = undefined;
     var dx: i64 = 0;
     var dy: i64 = 0;
+
     var visited = std.AutoHashMap(struct { i64, i64 }, void).init(allocator);
     defer visited.deinit();
     try visited.put(.{ dx, dy }, {});
 
     while (instructions.next()) |instruction| {
-        if (instruction[0] == 'R') {
-            direction = rotate_right(direction);
-        } else if (instruction[0] == 'L') {
-            direction = rotate_left(direction);
-        } else {
-            std.debug.panic("failed to parse instruction: {s}\n", .{instruction});
-        }
-
-        var distance = try std.fmt.parseInt(i64, instruction[1..], 10);
-
+        direction, distance = try parseInstruction(instruction, direction);
         while (distance > 0) {
             switch (direction) {
                 Direction.up => dy = dy + 1,
@@ -86,7 +84,7 @@ fn part2(buffer: []const u8, allocator: std.mem.Allocator) !u64 {
             distance = distance - 1;
         }
     }
-    std.debug.panic("not location was visited twice", .{});
+    std.debug.panic("no location was visited twice", .{});
 }
 
 pub fn main() !void {
